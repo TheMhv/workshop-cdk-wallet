17a235c17a25e719a2c678cfebd1dabb5cdf9c40
TheMhv <TheMhv@proton.me>
2026-09-18 00:18:54 -0300

    feat(cli): create cli commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, env, str::FromStr, sync::Arc, time::Duration};
+use std::{env, str::FromStr, sync::Arc, time::Duration};
 
 use anyhow::Ok;
 use cdk::wallet::{HttpClient, MeltQuote, MintConnector, MintQuote, SendOptions, Wallet};
@@ -6,13 +6,13 @@ use cdk_common::{
     Amount, CurrencyUnit, MintInfo, MintQuoteState, Proofs, Token, amount::SplitTarget,
     mint_url::MintUrl,
 };
-use cdk_sqlite::{WalletSqliteDatabase, wallet::memory};
-use lnd_grpc_rust::lnrpc::{Invoice, Payment, invoice};
-use rand::{RngExt, random, rng};
+use cdk_sqlite::WalletSqliteDatabase;
+use lnd_grpc_rust::lnrpc::Payment;
 use tokio::time::sleep;
 
 use crate::lnd::LND;
 
+mod cli;
 mod lnd;
 
 async fn mint_info(mint_url: &MintUrl) -> anyhow::Result<MintInfo> {
@@ -35,21 +35,21 @@ async fn create_wallet(
     )?)
 }
 
-async fn mint_quote(mint_info: &MintInfo, wallet: &Wallet) -> anyhow::Result<MintQuote> {
+async fn mint_quote(
+    mint_info: &MintInfo,
+    wallet: &Wallet,
+    amount: Amount,
+) -> anyhow::Result<MintQuote> {
     let payment_method = mint_info.nuts.nut04.methods.first().unwrap().method.clone();
 
     Ok(wallet
-        .mint_quote(payment_method, Some(Amount::from(21)), None, None)
+        .mint_quote(payment_method, Some(amount), None, None)
         .await?)
 }
 
-async fn pay_mint_quote(
-    wallet: &Wallet,
-    quote: &MintQuote,
-    payment: Payment,
-) -> anyhow::Result<()> {
+async fn pay_mint_quote(wallet: &Wallet, quote_id: &str, payment: Payment) -> anyhow::Result<()> {
     loop {
-        let status = wallet.check_mint_quote_status(&quote.id).await?;
+        let status = wallet.check_mint_quote_status(&quote_id).await?;
         match status.state {
             MintQuoteState::Paid => {
                 println!("Invoice paid!");
@@ -67,8 +67,8 @@ async fn pay_mint_quote(
     Ok(())
 }
 
-async fn mint_tokens(wallet: &Wallet, quote: &MintQuote) -> anyhow::Result<Proofs> {
-    Ok(wallet.mint(&quote.id, SplitTarget::None, None).await?)
+async fn mint_tokens(wallet: &Wallet, quote_id: &str) -> anyhow::Result<Proofs> {
+    Ok(wallet.mint(&quote_id, SplitTarget::None, None).await?)
 }
 
 async fn create_token(wallet: &Wallet, amount: u64) -> anyhow::Result<Token> {
@@ -93,54 +93,16 @@ async fn melt_quote(
 async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
-    let mut lnd_node = LND::new().await?;
-    println!("LND node info: {:?}", lnd_node.get_info().await?);
-
     let mint_url: MintUrl =
-        MintUrl::from_str(&env::var("MINT_URL").unwrap_or("http://localhost:8085".to_string()))?;
+        MintUrl::from_str(&env::var("MINT_URL").expect("Unable to set MINT URL"))?;
 
-    // Get mint info
-    let mint_info: MintInfo = mint_info(&mint_url).await?;
+    let macaroon_path = env::var("LND_MACAROON_PATH").expect("Unable to find LND macaroon file");
 
-    // Setup the wallet
-    let walletdb: WalletSqliteDatabase = memory::empty().await?;
-    let seed: [u8; 64] = random();
-    let wallet: Wallet = create_wallet(&mint_url, CurrencyUnit::Sat, walletdb, seed).await?;
+    let cert_path = env::var("LND_CERT_PATH").expect("Unable to find LND cert file");
 
-    // Minting tokens
-    let quote: MintQuote = mint_quote(&mint_info, &wallet).await?;
-    let payment = lnd_node.pay_invoice(quote.clone().request).await?;
-    pay_mint_quote(&wallet, &quote, payment).await.ok();
-    let proofs: Proofs = mint_tokens(&wallet, &quote).await?;
-    println!("Minted proofs: {:?}", proofs);
+    let lnd_url = env::var("LND_URL").expect("Unable to find LND URL server");
 
-    // Creating tokens
-    let balance: Amount = wallet.total_balance().await?;
-    let amount = rng().random_range(..(balance.to_u64() / 4));
-    let token: Token = create_token(&wallet, amount).await?;
-    println!("Minted tokens: {:?}", token);
-    // send token to another user
+    let lnd_node = LND::new(macaroon_path, cert_path, lnd_url).await?;
 
-    // Melting tokens
-    let receive_invoice = lnd_node
-        .create_invoice(Invoice {
-            memo: "Paid by Mint".to_string(),
-            value: token.value()?.to_i64().unwrap(),
-            ..Default::default()
-        })
-        .await?;
-    let melt_quote: MeltQuote =
-        melt_quote(&wallet, mint_info, &receive_invoice.payment_request).await?;
-    println!("Melt Quote: {:?}", melt_quote);
-
-    let prepare = wallet.prepare_melt(&melt_quote.id, HashMap::new()).await?;
-    let melted = prepare.confirm().await?;
-    println!("Melted: {:?}", melted);
-
-    let melt_payment = lnd_node.get_transaction(receive_invoice.r_hash).await?;
-    println!("Melt payment: {:?}", melt_payment);
-
-    assert_eq!(melt_payment.state(), invoice::InvoiceState::Settled);
-
-    Ok(())
+    cli::cli(mint_url, lnd_node).await
 }
